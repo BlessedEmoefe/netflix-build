7cc46f33c40dfa53cf617324bf0bcc2ca23c5c6f
blessed <blessedemoefe1122334455@gmail.com>
2022-09-18 17:44:56 +0100

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC5l44ZmrG2JIVGYKePlUqZ5TWBue5m3t0",
+  authDomain: "netflix-build-fd31d.firebaseapp.com",
+  projectId: "netflix-build-fd31d",
+  storageBucket: "netflix-build-fd31d.appspot.com",
+  messagingSenderId: "536989585622",
+  appId: "1:536989585622:web:d9c2878f92b1c5b1c2f977",
+};
