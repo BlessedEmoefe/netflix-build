6a2442285886dae58c120ec22cff3a7ed3a0f3f1
blessed <blessedemoefe1122334455@gmail.com>
2022-09-20 23:49:17 +0100

    started building the login page

--- src/features/LoginScreen/LoginScreen.js
+++ src/features/LoginScreen/LoginScreen.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./LoginScreen.css";
-import LoginScreenBg from "../../assets/images/loginScreen-bg.jpg"
+import LogoImage from "../../assets/images/logo.png";
+import LoginScreenBg from "../../assets/images/loginScreen-bg.jpg";
 
 const LoginScreen = () => {
   return (
-    <div className='loginScreen'>
-        <div className="loginScreen__background">
-            <img className='loginScreen__logo' src={LoginScreenBg} alt="login_background" />
-        </div>
-    </div>
-  )
-}
+    <div className="loginScreen">
+      <div
+        className="loginScreen__background"
+        style={{
+          background: `linear-gradient(to top,rgba(0,0,0,0.9) 0,rgba(0,0,0,0) 60%,rgba(0,0,0,0.9) 100%), url(${LoginScreenBg})`,
+        }}
+      >
+        <img className="loginScreen__logo" src={LogoImage} alt="logo" />
 
-export default LoginScreen
+        <button className="loginScreen__button">Sign In</button>
+      </div>
+
+      <div className="loginScreen__body">
+        
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
