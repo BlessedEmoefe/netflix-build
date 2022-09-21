96b79bf8cc5c734ef819971ae7ade392d7689759
blessed <blessedemoefe1122334455@gmail.com>
2022-09-21 17:00:21 +0100

    commiting leftover code

--- src/features/LoginScreen/LoginScreen.js
+++ src/features/LoginScreen/LoginScreen.js
@@ -19,8 +19,18 @@ const LoginScreen = () => {
 
       <div className="loginScreen__body">
         <>
-        <h1>Unlimited films, Tv programmes and more.</h1>
-        <h2>Watch anywhere.cancek=l anytime</h2>
+        <h1>Unlimited movies, Tv shows and more.</h1>
+        <h2>Watch anywhere. Cancel anytime</h2>
+        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+        <div className="loginScreen__input">
+            <form>
+                <input type="email" placeholder="Email Address" />
+                <button className="loginScreen__getStarted">
+                    GET STARTED
+                </button>
+            </form>
+        </div>
         </>
       </div>
     </div>
